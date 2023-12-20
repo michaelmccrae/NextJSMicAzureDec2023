caa77732dcc8c4e3a277f94604f11f3298817195
michaelmccrae <mike@mccrae.ca>
2023-12-19 22:16:27 -0800

    first commit

---
+++ src/app/components/page.tsx
@@ -0,0 +1,14 @@
+import Image from 'next/image'
+import Micone from './micone'
+
+function page() {
+    return (
+     
+        <main>
+            <h1>Working</h1>
+            <Micone />
+        </main>
+    )
+}
+
+export default page;
