20d9014275d6ecb32a990cec40cd193c4a5e8186
michaelmccrae <mike@mccrae.ca>
2023-12-22 22:56:00 -0800

    Another next.config change for isolation

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const nextConfig = {
     async headers() {
       return [
         {
-          source: '/components/(.*)',
+          source: '/components',
           headers: [
             {
               key: 'Cross-Origin-Embedder-Policy',
